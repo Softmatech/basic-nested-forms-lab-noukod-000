b5b190e0e910497d15aec0cea21d57151e3d8497
Andy Feidje Joseph <ingandy75@gmail.com>
2019-08-14 17:52:54 +0000

    Automatically backed up by Learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
   # => accepts_nested_attributes_for  :ingredients
+  def ingredients_attributes=(ingredients_attributes)
+    ingredients_attributes.each do |i,  ingredients_attributes|
+      self.ingredients_attributes.build(ingredients_attributes)
+    end
+  end
 end
